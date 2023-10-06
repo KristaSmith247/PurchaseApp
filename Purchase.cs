ab142a7638b53dd6b49b67f8435027c1febe78ca
KristaSmith247 <118084776+KristaSmith247@users.noreply.github.com>
2023-10-05 19:53:13 -0600

    Add files via upload

---
+++ Purchase.cs
@@ -0,0 +1,86 @@
+﻿/* Purchase.cs
+ * Author: Krista Smith
+ * Date: 9/30/23
+ * Description: The class will create a Purchase object and provide a string
+ * override to print an object.
+ */
+
+namespace PurchaseApp
+{
+    internal class Purchase
+    {
+        // accessors and mutators
+        public decimal PurchaseAmount { get; set; }
+        public decimal SalesTax { get; set; }
+        public decimal Shipping { get; set; }
+        public decimal TotalSalePrice { get; set; }
+        public int NumberItemsPurchased { get; set; }
+
+        // constructors
+
+        public Purchase(decimal price)
+        {
+            PurchaseAmount = price;
+        }
+
+        // functions
+        public void DisplayInstructions()
+        {
+            //  display program instructions
+            Console.WriteLine("This application computes the total due for purchases.");
+            Console.WriteLine("You may enter any number of purchase amounts.");
+            Console.WriteLine("When you have finished, the total due will be displayed with sales tax and shipping charges included.");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        public decimal CalculateShipping(int numItems)
+        {
+            // calculate shipping based on number of items in purchase list
+            decimal shipping;
+            if (numItems < 3)
+            {
+                shipping = 3.50M;
+            }
+            else if (numItems < 7)
+            {
+                shipping = 5.00M;
+            }
+            else if (numItems < 11)
+            {
+                shipping = 7.00M;
+            }
+            else if (numItems < 15)
+            {
+                shipping = 9.00M;
+            }
+            else { shipping = 10.00M; }
+
+            return shipping;
+        }
+
+        public decimal CalculateSalesTax(decimal totalAmount)
+        {
+            // calculate sales tax by multiplying total by 7.75%
+            return totalAmount * 0.0775M;
+        }
+
+        public decimal CalculateTotalPayment(decimal itemPayment, decimal shipPrice, decimal taxes)
+        {
+            // calculate grand total by adding purchases, shipping, and sales tax
+            return itemPayment + shipPrice + taxes;
+        }
+
+        public override string ToString()
+        { 
+            // print the object receipt
+            return "-----------------------------------" + "\n\tSales Receipt" +
+                "\nTotal Purchases: " + PurchaseAmount.ToString("c").PadLeft(16) +
+                "\nSales Tax: " + SalesTax.ToString("c").PadLeft(22) +
+                "\nNumber of Items Purchased: " + NumberItemsPurchased.ToString().PadLeft(6)
+                + "\nShipping Charge: " + Shipping.ToString("c").PadLeft(16) +
+                "\n-----------------------------------" +
+                "\nGrand Total: " + TotalSalePrice.ToString("C").PadLeft(20);
+        }
+    }
+}
---
+++ PurchaseApp.cs
@@ -0,0 +1,126 @@
+﻿/* PurchaseApp.cs
+ * Author: Krista Smith
+ * Date: 9.30.23
+ * Description: The PurchaseApp.cs will test a Purchase object by
+ * computing the total due for purchases, sales tax and shipping charges.
+ */
+
+using static System.Console;
+
+namespace PurchaseApp
+{
+    public class Purchases
+    {
+        static void Main(string[] args)
+        {
+            char anotherItem = 'N'; // sentinel value initialized to N
+            string inValue;     // value to parse input
+            decimal itemPrice = 0;  // input price from user
+
+            Purchase receipt = new Purchase(itemPrice);
+            receipt.DisplayInstructions();
+            Clear();
+
+            do
+            {
+                GetItemPrice(out itemPrice); // get purchase amount from user
+
+                receipt.NumberItemsPurchased += 1; // increment number of items
+                receipt.PurchaseAmount += itemPrice; // sum purchase prices
+
+                WriteLine("Would you like to enter another item? (Y/N)");
+                inValue = ReadLine();
+                anotherItem = Convert.ToChar(inValue);
+            }
+            while ((anotherItem == 'Y') || (anotherItem == 'y'));
+            // Once user has entered all values:
+            // calculate shipping on total purchase
+            receipt.Shipping = receipt.CalculateShipping(receipt.NumberItemsPurchased);
+            
+            // calculate sales tax on total
+            receipt.SalesTax = receipt.CalculateSalesTax(receipt.PurchaseAmount);
+            
+            // calculate grand total
+            receipt.TotalSalePrice = receipt.CalculateTotalPayment(receipt.PurchaseAmount, receipt.Shipping, receipt.SalesTax);
+            
+            // display receipt
+            WriteLine(receipt);
+
+        }
+
+        private static decimal GetItemPrice(out decimal itemPrice)
+        {   // ask user for item price
+            string input;
+            WriteLine("Please enter the price of the item: ");
+            input = ReadLine();
+            while (decimal.TryParse(input, out itemPrice) == false || itemPrice <= 0)
+            {
+                // check to make sure user entered a valid input
+                WriteLine("You entered an incorrect value. Please enter a non-negative number: ");
+                input = ReadLine();
+            }
+            return itemPrice;
+        }
+    }
+}
+
+//int numberOfItems = 0;  // number of items in purchase
+//decimal totalPrice = 0; // total due with sales tax and shipping
+// decimal totalTax = 0; // sales tax on purchase
+
+
+// before changes: to string is the only function in purchase.cs
+
+// things that worked but are probably unneeded: 
+
+// increment item count:
+// numberOfItems += 1;
+// receipt.NumberItemsPurchased = AddItem(numberOfItems); // add 1 to number of items
+
+//private static int AddItem(int number)
+//{
+//    // add one for each time the function is called
+//    return number++;
+//}
+
+
+//private static decimal CalculateShipping(int number)
+//{
+//    int numItems = number;
+//    decimal shipping;
+
+//    if (numItems < 3)
+//    {
+//        shipping = 3.50M;
+//    }
+//    else if (numItems < 7)
+//    {
+//        shipping = 5.00M;
+//    }
+//    else if (numItems < 11)
+//    {
+//        shipping = 7.00M;
+//    }
+//    else if (numItems < 15)
+//    {
+//        shipping = 9.00M;
+//    }
+//    else { shipping = 10.00M; }
+
+//    return shipping;
+//}
+
+//public static decimal CalculateSalesTax(decimal totalAmount)
+//{
+
+//    return totalAmount * 0.075M;
+//}
+
+//receipt.Shipping = receipt.CalculateShipping(receipt.NumberItemsPurchased); // increase shipping price based on number of items
+
+//receipt.TotalSalePrice += itemPrice; // sum grand total
+//totalPrice += receipt.TotalSalePrice; // sum grand total
+//totalTax = CalculateSalesTax(itemPrice); // calculate sales tax per item
+//receipt.SalesTax += totalTax; // sum total tax for entire purchase
+//receipt.TotalSalePrice += receipt.PurchaseAmount + receipt.SalesTax;
+//WriteLine(receipt);
